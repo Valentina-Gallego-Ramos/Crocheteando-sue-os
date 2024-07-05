9ae148c535de96643af2ee9e2a9e297e44ec50e8
Valentina-Gallego-Ramos <144750192+Valentina-Gallego-Ramos@users.noreply.github.com>
2024-07-05 16:07:59 -0500

    Update compra.js

--- compra.js
+++ compra.js
@@ -24,7 +24,7 @@ function procesarCompra(e){
             timer: 2500,
             showConfirmButton: false
           }).then(function(){
-              window.location = "productos.html";
+              window.location = "products.html";
           })
     }
     else if(cliente.value === '' || correo.value === ''){
